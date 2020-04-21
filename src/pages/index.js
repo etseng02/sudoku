0764097e0f51dc978fa00f25125b7d7ae3c21782
Eddie <eddietseng@outlook.com>
2020-04-21 11:27:39 -0400

    Fix - Added back fragment to Index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import '../styles/index.scss'
-import Square from '../components/Square'
+
+import Square from '../components/Square.js'
 
 
 const Index = () => {
   return (
+    <Fragment>
     <div className = "square-grid">
             <Square/>
             <Square/>
@@ -16,6 +18,7 @@ const Index = () => {
             <Square/>
             <Square/>
     </div>
+    </Fragment>
   );
 }
 
