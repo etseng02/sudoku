20b266997f1ccb9c721a2847a70a911f833463d7
Eddie <eddietseng@outlook.com>
2020-04-21 10:23:08 -0400

    Added structure for first set of 9 boxes

--- src/components/square.js
+++ src/components/square.js
@@ -5,15 +5,19 @@ const Square = () => {
 
 
   return (
-    <div className="square">
-      <div className="content">
-          <div className="table">
-              <div className="table-cell">
-                  Square
-              </div>
-          </div>
-      </div>
-    </div>
+<div className="container">
+  <div className="grid">
+    <div className="cell" id="1">1</div>
+    <div className="cell" id="2">2</div>
+    <div className="cell" id="3">3</div>
+    <div className="cell" id="4">4</div>
+    <div className="cell" id="5">5</div>
+    <div className="cell" id="6">6</div>
+    <div className="cell" id="7">7</div>
+    <div className="cell" id="8">8</div>
+    <div className="cell" id="9">9</div>
+  </div>
+</div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import '../styles/index.scss'
 import Square from '../components/square'
 
 const Index = () => {
   return (
     <div className = "main-page">
-      <Square/>
+
+
+      <Fragment>
+
+            <Square/>
+
+      </Fragment>
+
+
     </div>
   );
 }
