e9e0a1f37aed37ede0938cc41f75e37ca4ea4ee0
Eddie <eddietseng@outlook.com>
2020-04-21 14:02:42 -0400

    Added state to pages and passed down state as prop to Square

--- src/components/square.js
+++ src/components/square.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../styles/square.scss'
 
 const Square = (props) => {
+  
+  const [number, setNumber] = useState({
+    one: null
+  });
+
+  useEffect(()=>{
+    if (props.data) {
+      setNumber(number => ({
+        ...number,
+        one: props.data[0][0],
+        two: props.data[0][1],
+        three: props.data[0][2],
+        four: props.data[1][0],
+        five: props.data[1][1],
+        six: props.data[1][2],
+        seven: props.data[2][0],
+        eight: props.data[2][1],
+        nine: props.data[2][2],
+      }))
+    }
+    },[props])
 
 
   return (
     <div className="container">
       <div className="grid">
-        <div className="cell" id="1">1</div>
-        <div className="cell" id="2">2</div>
-        <div className="cell" id="3">3</div>
-        <div className="cell" id="4">4</div>
-        <div className="cell" id="5">5</div>
-        <div className="cell" id="6">6</div>
-        <div className="cell" id="7">7</div>
-        <div className="cell" id="8">8</div>
-        <div className="cell" id="9">9</div>
+        <div className="cell" id="1">{number.one}</div>
+        <div className="cell" id="2">{number.two}</div>
+        <div className="cell" id="3">{number.three}</div>
+        <div className="cell" id="4">{number.four}</div>
+        <div className="cell" id="5">{number.five}</div>
+        <div className="cell" id="6">{number.six}</div>
+        <div className="cell" id="7">{number.seven}</div>
+        <div className="cell" id="8">{number.eight}</div>
+        <div className="cell" id="9">{number.nine}</div>
       </div>
     </div>
   )
 }
+
+
 
 
 export default Square
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import '../styles/index.scss'
 
 import Square from '../components/Square.js'
 
 
 const Index = () => {
+
+  const [state, setState] = useState({
+    block1: [[1, 0, 4],[0, 0,0],[0, 0,1]],
+    block2: [[null, null,null],[null, null,null],[null, null,null]],
+    block3: [[null, null,null],[null, null,null],[null, null,null]],
+    block4: [[null, null,null],[null, null,null],[null, null,null]],
+    block5: [[null, null,null],[null, null,null],[null, null,null]],
+    block6: [[null, null,null],[null, null,null],[null, null,null]],
+    block7: [[null, null,null],[null, null,1],[null, null,null]],
+    block8: [[null, null,null],[null, null,null],[null, null,null]],
+    block9: [[null, null,null],[null, null,null],[null, null,null]],
+  });
+
   return (
     <Fragment>
     <div className = "square-grid">
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
-            <Square/>
+            <Square data={state.block1}/>
+            <Square data={state.block2}/>
+            <Square data={state.block3}/>
+            <Square data={state.block4}/>
+            <Square data={state.block5}/>
+            <Square data={state.block6}/>
+            <Square data={state.block7}/>
+            <Square data={state.block8}/>
+            <Square data={state.block9}/>
     </div>
     </Fragment>
   );
