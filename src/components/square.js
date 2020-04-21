9f50fd6aed386c58423604e768481167dc18d203
Eddie <eddietseng@outlook.com>
2020-04-20 22:21:54 -0400

    initial commit

---
+++ src/components/square.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/square.scss'
+
+const Square = () => {
+
+
+  return (
+    <div className="square">
+      <div className="content">
+          <div className="table">
+              <div className="table-cell">
+                  Square
+              </div>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default Square
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/index.scss'
+import Square from '../components/square'
+
+const Index = () => {
+  return (
+    <div className = "main-page">
+      <Square/>
+    </div>
+  );
+}
+
+export default Index;
